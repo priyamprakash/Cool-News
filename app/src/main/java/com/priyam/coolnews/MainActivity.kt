c25b013760572e69929a31d869d8e9878648c927
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2022-11-27 03:17:25 +0530

    Theme

--- app/src/main/java/com/priyam/coolnews/MainActivity.kt
+++ app/src/main/java/com/priyam/coolnews/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.priyam.coolnews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
