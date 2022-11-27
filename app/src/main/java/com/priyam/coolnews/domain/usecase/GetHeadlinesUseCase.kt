a1672d15e633f6ae5c0b5e4129ad556bd827566e
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2022-11-28 01:12:40 +0530

    Use Cases
    Headlines
    Searched News

--- app/src/main/java/com/priyam/coolnews/domain/usecase/GetHeadlinesUseCase.kt
+++ app/src/main/java/com/priyam/coolnews/domain/usecase/GetHeadlinesUseCase.kt
@@ -1,6 +1,11 @@
 package com.priyam.coolnews.domain.usecase
 
+import com.priyam.coolnews.data.model.APIResponse
+import com.priyam.coolnews.data.util.Resource
 import com.priyam.coolnews.domain.repository.NewsRepository
 
 class GetHeadlinesUseCase(private val newsRepository: NewsRepository) {
+    suspend fun execute(): Resource<APIResponse>{
+        return  newsRepository.getNewsHeadlines()
+    }
 }
--- app/src/main/java/com/priyam/coolnews/domain/usecase/GetSearchedNewsUseCase.kt
+++ app/src/main/java/com/priyam/coolnews/domain/usecase/GetSearchedNewsUseCase.kt
@@ -1,6 +1,11 @@
 package com.priyam.coolnews.domain.usecase
 
+import com.priyam.coolnews.data.model.APIResponse
+import com.priyam.coolnews.data.util.Resource
 import com.priyam.coolnews.domain.repository.NewsRepository
 
 class GetSearchedNewsUseCase(private val newsRepository: NewsRepository) {
+    suspend fun execute(searchQuery:String): Resource<APIResponse>{
+        return  newsRepository.getSearchedNews(searchQuery)
+    }
 }
