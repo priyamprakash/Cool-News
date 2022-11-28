87217b78a268881e47449597c162a25e75d52793
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2022-11-28 23:47:29 +0530

    Create NetModule.kt

---
+++ app/src/main/java/com/priyam/coolnews/presentation/di/NetModule.kt
@@ -0,0 +1,11 @@
+package com.priyam.coolnews.presentation.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetModule {
+}
