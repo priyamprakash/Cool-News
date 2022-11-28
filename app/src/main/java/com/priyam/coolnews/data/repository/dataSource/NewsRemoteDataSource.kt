dab269f09db914f501fed564e9918a753515d631
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2022-11-29 01:45:23 +0530

    Revert "Repo Module"
    This reverts commit 78a56be1d2bdb16f65231581c6041434580b6a29.

--- app/src/main/java/com/priyam/coolnews/data/repository/dataSource/NewsRemoteDataSource.kt
+++ app/src/main/java/com/priyam/coolnews/data/repository/dataSource/NewsRemoteDataSource.kt
@@ -4,7 +4,5 @@ import com.priyam.coolnews.data.model.APIResponse
 import retrofit2.Response
 
 interface NewsRemoteDataSource {
-    suspend fun getTopHeadlines(country : String, page : Int):Response<APIResponse>
-    suspend fun getSearchedNews(country : String,searchQuery:String, page : Int):Response<APIResponse>
-
+    suspend fun getTopHeadlines():Response<APIResponse>
 }
