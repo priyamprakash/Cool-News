6aa7c1fcfe9c0fdf939a5254bd3762e51914d54c
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2022-11-28 23:48:38 +0530

    Update NetModule.kt

--- app/src/main/java/com/priyam/coolnews/presentation/di/NetModule.kt
+++ app/src/main/java/com/priyam/coolnews/presentation/di/NetModule.kt
@@ -1,11 +1,31 @@
 package com.priyam.coolnews.presentation.di
 
+import com.priyam.coolnews.data.api.NewsApiService
+import com.priyam.coolnews.data.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.internal.managers.ApplicationComponentManager
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetModule {
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constants.url)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideNewsAPIService(retrofit: Retrofit):NewsApiService{
+        return retrofit.create(NewsApiService::class.java)
+    }
 }
