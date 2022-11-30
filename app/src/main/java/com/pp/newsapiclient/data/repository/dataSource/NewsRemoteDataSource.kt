670969cd36fe0713bff40f4bc1bb0e8b165e4610
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2022-11-30 23:48:19 +0530

    Data Sources Interfaces Removed

--- app/src/main/java/com/pp/newsapiclient/data/repository/dataSource/NewsRemoteDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.pp.newsapiclient.data.repository.dataSource
-
-import com.pp.newsapiclient.data.model.APIResponse
-import retrofit2.Response
-
-interface NewsRemoteDataSource {
-    suspend fun getTopHeadlines(country : String, page : Int):Response<APIResponse>
-    suspend fun getSearchedNews(country : String,searchQuery:String, page : Int):Response<APIResponse>
-}
