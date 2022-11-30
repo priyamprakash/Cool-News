ffc3300d5a41a875008304b9c5e130cd8cb4f85f
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2022-11-30 19:12:35 +0530

    Deleted All

--- app/src/main/java/com/priyam/coolnews/data/repository/dataSource/NewsRemoteDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.priyam.coolnews.data.repository.dataSource
-
-import com.priyam.coolnews.data.model.APIResponse
-import retrofit2.Response
-
-interface NewsRemoteDataSource {
-    suspend fun getTopHeadlines():Response<APIResponse>
-}
--- app/src/main/java/com/priyam/coolnews/domain/repository/NewsRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.priyam.coolnews.domain.repository
-
-import com.priyam.coolnews.data.model.APIResponse
-import com.priyam.coolnews.data.util.Resource
-
-interface NewsRepository {
-    suspend fun getNewsHeadlines(): Resource<APIResponse>
-    suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse>
-}
